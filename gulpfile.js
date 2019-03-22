e29576bcc68ec8fdecff1212456a7d7a8a17e1aa
val <val.antonini@gmail.com>
2019-03-22 21:57:17 +1000

    improved gulp, layout, colors

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,47 @@
-const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-const ts = require('gulp-typescript');
+const tsc = require('gulp-typescript');
 const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
 const {
     src,
     dest,
+    series,
     parallel,
-    series
+    watch
 } = require('gulp');
 
 const purge = () => {
-    return src('contents/scripts/site.min.js')
-    .pipe(clean({force: true}));
+    return src('contents/scripts/site.min.js', {
+            allowEmpty: true
+        }, 'contents/styles/site.min.css', {
+            allowEmpty: true
+        })
+        .pipe(clean({
+            force: true
+        }));
 }
 
-const js = () => {
-    return src('contents/scripts/*.js', {
+const scss = () => {
+    return src('contents/styles/site.scss')
+        .pipe(concat('site.min.css'))
+        .pipe(sass({
+            outputStyle: 'compressed',
+            "includePaths": [
+                "node_modules/",
+                "content/styles"
+            ]
+        }).on('error', sass.logError))
+        .pipe(dest('contents/styles'))
+}
+
+const ts = () => {
+    return src('contents/scripts/*.ts', {
             sourcemaps: false
         })
+        .pipe(tsc())
         .pipe(concat('site.min.js'))
         .pipe(uglify())
         .pipe(dest('contents/scripts', {
@@ -27,5 +50,13 @@ const js = () => {
 
 };
 
-exports.js = js;
-exports.default = series(purge,js);
+exports.ts = ts;
+exports.default =  () => {
+    series(purge)();
+    parallel(ts, scss)();
+}
+
+watch(['contents/scripts/*.ts', 'contents/styles/*.scss'], (cb) => {
+    exports.default();
+    cb();
+});
